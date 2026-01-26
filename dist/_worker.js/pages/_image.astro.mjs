6878cb05a586170d8eabd02ecd57ee45061e7759
André Lima <allima97@gmail.com>
2026-01-26 12:16:19 -0300

    melhorias-ajustes-segurança

--- dist/_worker.js/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { p as page } from '../chunks/generic_CzidszyX.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_MjSq-9QN.mjs';
